771a677250144d77cb75b9c910fe066bc2d5df9b
yuriykvyatkovskyy <yuriykreik20021997@gmail.com>
2023-10-16 19:33:58 +0300

    commit

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,4 @@
 function getElementWidth(content, padding, border) {
-    // return content + padding + border
     return Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2
 }
 
