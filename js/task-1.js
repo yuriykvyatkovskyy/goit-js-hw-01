02caf36e71f88fd5b3f5b0e1e0706d909e7b25c5
yuriykvyatkovskyy <yuriykreik20021997@gmail.com>
2023-10-16 19:17:30 +0300

    create task-1 and task-2

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,7 @@
 function makeTransaction(quantity, pricePerDroid) {
-    console.log(`You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`)
+    return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`
 }
 
-makeTransaction(5, 3000); 
-makeTransaction(3, 1000); 
-makeTransaction(10, 500); 
+console.log(makeTransaction(5, 3000)); 
+console.log(makeTransaction(3, 1000)); 
+console.log(makeTransaction(10, 500));
